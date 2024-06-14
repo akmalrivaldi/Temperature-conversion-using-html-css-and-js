73b1183a93db4e9b59b261503258d566b51c60d6
akmalrivaldi <akmallrival333@gmail.com>
2024-06-14 09:46:25 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+
+function convert() {
+  let temp;
+
+  if (toFahrenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (toCelcius.checked) {
+    temp = Number(textBox.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent = temp.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Select a unit";
+  }
+}
+
+function hapus() {
+  document.getElementById("textBox").value = "";
+  toFahrenheit.checked = false;
+  toCelcius.checked = false;
+  document.getElementById("result").textContent = "";
+}
